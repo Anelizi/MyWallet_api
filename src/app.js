dd203b261d62ae4d28e6a8cdd2afe94976bcdceb
Anelizi <anelizicarvalho@gmail.com>
2023-04-20 22:38:20 -0300

    iniciando: POST /transactions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { json } from "express";
-import Joi from "joi";
+import express from "express";
+import Joi, { object } from "joi";
 import { MongoClient } from "mongodb";
 import { v4 as uuid } from "uuid";
 
@@ -23,11 +23,17 @@ try {
 }
 const db = mongoClient.db();
 
-// Schemas
 const userSchemas = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().min(3),
   email: Joi.string().email().required(),
   password: Joi.string().required().min(3),
+});
+
+const transactionsSchemas = Joi.object({
+  type: Joi.string().valid("green", "red"),
+  value: Joi.string(),
+  description: Joi.string().max(20),
+  date: Joi.string().min(5).max(5).required(),
 });
 
 app.post("/sign-up", async (req, res) => {
@@ -41,7 +47,7 @@ app.post("/sign-up", async (req, res) => {
 
   try {
     const user = await db.collection("users").findOne({ email });
-    if (user) return res.status(409).send("E-mail já cadastrado.");
+    if (user) return res.sendStatus(409);
 
     const hash = bcrypt.hashSync(password, 10);
 
@@ -57,10 +63,10 @@ app.post("/sign-in", async (req, res) => {
 
   try {
     const user = await db.collection("users").findOne({ email });
-    if (!user) return res.status(401).send("E-mail inválido.");
-    if (!email || !password) {
-      return res.status(422).send("E-mail e senha são obrigatórios.");
-    }
+
+    if (!user) return res.status(404).send("E-mail não está cadastrado");
+
+    if (!email || !password) return res.sendStatus(422);
 
     const passwordCorrect = bcrypt.compareSync(password, user.password);
     if (!passwordCorrect) return res.status(401).send("Senha incorreta.");
@@ -68,7 +74,37 @@ app.post("/sign-in", async (req, res) => {
     const token = uuid();
     await db.collection("sessions").insertOne({ token, idUser: user._id });
 
-    res.status(200).send(token);
+    res.send(token);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post("/transactions", async (req, res) => {
+  const { type, value, description, date } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  const validation = userSchemas.validate(res.body, { abortEarly: false });
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) return res.sendStatus(401);
+
+    const information = {
+      type,
+      description,
+      date,
+      value: Number(value.replace(",", ".")).toFixed(2),
+    };
+
+    res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error.message);
   }
