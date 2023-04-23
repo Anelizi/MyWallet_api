a0d859cb146c5ec4e5522737adef6095a1da7a40
Anelizi <anelizicarvalho@gmail.com>
2023-04-23 20:11:15 -0300

    requisição: POST /transactions e GET /sign-up

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
 import cors from "cors";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 import express from "express";
-import Joi, { object } from "joi";
-import { MongoClient } from "mongodb";
+import Joi from "joi";
+import { MongoClient, ObjectId } from "mongodb";
 import { v4 as uuid } from "uuid";
 
 const app = express();
@@ -30,10 +31,10 @@ const userSchemas = Joi.object({
 });
 
 const transactionsSchemas = Joi.object({
-  type: Joi.string().valid("green", "red"),
+  type: Joi.string().valid("entrada", "saida"),
   value: Joi.string(),
-  description: Joi.string().max(20),
-  date: Joi.string().min(5).max(5).required(),
+  description: Joi.string().max(30),
+  date: Joi.string(),
 });
 
 app.post("/sign-up", async (req, res) => {
@@ -51,7 +52,9 @@ app.post("/sign-up", async (req, res) => {
 
     const hash = bcrypt.hashSync(password, 10);
 
-    await db.collection("users").insertOne({ name, email, password: hash });
+    await db
+      .collection("users")
+      .insertOne({ name, email, password: hash, money: 0 });
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error.message);
@@ -80,14 +83,27 @@ app.post("/sign-in", async (req, res) => {
   }
 });
 
+app.get("/sign-up", async (req, res) => {
+  try {
+    const user = await db.collection("users").find().toArray();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 app.post("/transactions", async (req, res) => {
+  const time = dayjs().format("DD/MM");
   const { type, value, description, date } = req.body;
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
   if (!token) return res.sendStatus(401);
 
-  const validation = userSchemas.validate(res.body, { abortEarly: false });
+  const validation = transactionsSchemas.validate({type, value, description, date}, {
+    abortEarly: false,
+  });
+
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
     return res.status(422).send(errors);
@@ -97,18 +113,47 @@ app.post("/transactions", async (req, res) => {
     const session = await db.collection("sessions").findOne({ token });
     if (!session) return res.sendStatus(401);
 
-    const information = {
+    const userId = session.id;
+
+    console.log(userId);
+
+    const user = await db
+      .collection("users")
+      .findOne({ _id:new ObjectId(userId) });
+
+    const informatio = {
       type,
       description,
-      date,
+      date: time,
       value: Number(value.replace(",", ".")).toFixed(2),
+      userId
     };
 
-    res.sendStatus(201);
+    if (informatio.type === "entrada") {
+      await db
+        .collection("users")
+        .updateOne(
+          { _id: new ObjectId(userId) },
+          { $set: { money: Number(user?.money || 0) + Number(informatio.value)}}
+        );
+    } else {
+      await db
+        .collection("users")
+        .updateOne(
+          { _id: new ObjectId(userId) },
+          {
+            $set: { money: (Number(user?.money || 0) - Number(informatio.value)).toFixed(2)}}
+        );
+    }
+
+    await db.collection("information").insertOne(informatio);
+
+    res.status(201).send(informatio);
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
