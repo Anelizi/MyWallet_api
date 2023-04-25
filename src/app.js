3248923fdc88d2e43cdd1f42e2270d7615ababbb
Anelizi <anelizicarvalho@gmail.com>
2023-04-24 21:30:10 -0300

    conectando: mongodb atlas

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
